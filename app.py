a52c62e4e2b28b4014066fe8f57593cc39b67180
gabrielcfs1 <colombogabriel00@gmail.com>
2022-11-14 21:16:29 -0300

    first commit email_sender

---
+++ app.py
@@ -0,0 +1,31 @@
+from email.message import EmailMessage
+from password import password
+import ssl 
+import smtplib 
+
+email_sender = ''
+email_password = password
+
+
+email_receiver = ''
+
+# ASSUNTO
+subject = 'TESTE EMAIL SENDER PYTHON'
+# CORPO DO EMAIL 
+body = """
+TESTANDO CORPO EMAIL SENDER PYTHON
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver  
+em['Subject'] = subject
+em.set_content(body)
+
+# CREATE SSL CONTEXT
+context = ssl.create_default_context()
+
+# CONNECTION WITH SMTP SERVER 
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
